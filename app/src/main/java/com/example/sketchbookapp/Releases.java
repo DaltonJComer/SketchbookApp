7145c5e5e6cbcaf96db9429ebb5edaac4e76275a
DaltonJComer <dalton.comer@outlook.com>
2020-12-04 13:59:40 -0500

    This is the Tentative build of the sketchbook app. Still requires internet checks and email sending services for requesting comics.

--- app/src/main/java/com/example/sketchbookapp/Releases.java
+++ app/src/main/java/com/example/sketchbookapp/Releases.java
@@ -2,19 +2,66 @@ package com.example.sketchbookapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+import static android.widget.Toast.LENGTH_SHORT;
 
 public class Releases extends AppCompatActivity {
-
+    public static ListView data;
+    public static String[] listItems;
+    private DBHelper dbhelper = new DBHelper(this);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_releases);
+
+        data = (ListView) findViewById(R.id.data);
+
+        fetchData process = new fetchData();
+        try {
+            Void result = process.execute().get();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        listItems =process.arrayList;
+
+        final ArrayList<String> list = new ArrayList<String>();
+        for (int i = 0; i < listItems.length; ++i) {
+            list.add(listItems[i]);
+        }
+
+        final StableArrayAdapter listAdapter = new StableArrayAdapter(this,android.R.layout.simple_list_item_1, list);
+        data.setAdapter(listAdapter);
+
+        /*ArrayAdapter<CharSequence> aa = ArrayAdapter.createFromResource(this, listItems, android.R.layout.simple_list_item_1);
+        data.setAdapter(aa);*/
+
+        //data.setText(listItems);
+        //System.out.println(listItems);
 
         Spinner menu = findViewById(R.id.menu);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.menu_options, android.R.layout.simple_spinner_item);
@@ -42,6 +89,9 @@ public class Releases extends AppCompatActivity {
                             intent = new Intent(getBaseContext(),Events.class);
                             startActivity(intent);
                             break;
+                        case "Request Comics":
+                            intent = new Intent(getBaseContext(), RequestForm.class);
+                            startActivity(intent);
                     }
                 }
             }
@@ -51,5 +101,60 @@ public class Releases extends AppCompatActivity {
 
             }
         });
+
+        data.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                final String comicDetails = adapterView.getItemAtPosition(position).toString();
+                String comicName = comicDetails.split("\n")[0];
+                System.out.println("SELECTED:"+comicName);
+                AlertDialog.Builder builder = new AlertDialog.Builder(Releases.this,R.style.MyAlertDialogStyle);
+
+                builder.setTitle("Add to watchlist?");
+
+                builder.setMessage("Add "+comicName+" to wish list?");
+                builder.setPositiveButton("Add to watchlist", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dbhelper.addData(comicDetails.split("\n"));
+                    }
+                });
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            }
+        });
     }
+
+    private class StableArrayAdapter extends ArrayAdapter<String> {
+
+        HashMap<String, Integer> mIdMap = new HashMap<String, Integer>();
+
+        public StableArrayAdapter(Context context, int textViewResourceId,
+                                  List<String> objects) {
+            super(context, textViewResourceId, objects);
+            for (int i = 0; i < objects.size(); ++i) {
+                mIdMap.put(objects.get(i), i);
+            }
+        }
+
+        @Override
+        public long getItemId(int position) {
+            String item = getItem(position);
+            return mIdMap.get(item);
+        }
+
+        @Override
+        public boolean hasStableIds() {
+            return true;
+        }
+
+    }
+
 }
